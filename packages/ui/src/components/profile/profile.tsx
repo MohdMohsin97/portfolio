86cd2740eb4ff6bf2bacaab68f6624340cf22027
Mohd Mohsin <mohsinmohd9718@gmail.com>
2023-12-04 22:10:11 +0530

    added tailwind

---
+++ packages/ui/src/components/profile/profile.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/function-component-definition */
+
+export const Profile = (): JSX.Element => {
+  return (
+    <div className="object-cover">
+        <img alt="profile" className="object-cover" src="/assests/profile.jpg" width={100} height={100} />
+      
+    </div>
+  );
+};
