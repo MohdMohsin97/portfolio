3ccfcc82282ec52842438020a2dbf0b2dacf63b6
Mohd Mohsin <mohsinmohd9718@gmail.com>
2024-01-10 22:25:04 +0530

    deployed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
